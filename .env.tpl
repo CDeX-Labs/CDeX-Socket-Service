134ad3a5287893ba4379854e483e610557790cfa
NotAditya69 <90441096+NotAditya69@users.noreply.github.com>
2024-04-20 14:50:37 +0530

    feat(initial): first commit

---
+++ .env.tpl
@@ -0,0 +1,2 @@
+APP_NAME="SocketService"
+PORT=6001
---
+++ pkg/socket/socket.go
@@ -0,0 +1,46 @@
+package socket
+
+import (
+    "net/http"
+    "github.com/gorilla/websocket"
+    "github.com/rs/zerolog/log"
+)
+
+var upgrader = websocket.Upgrader{
+    CheckOrigin: func(r *http.Request) bool {
+        return true
+    },
+}
+
+func HandleWebSocket(w http.ResponseWriter, r *http.Request) {
+    conn, err := upgrader.Upgrade(w, r, nil)
+    if err != nil {
+        log.Error().Err(err).Msg("Error upgrading connection to websocket")
+        return
+    }
+    defer conn.Close()
+
+    log.Debug().Msg("Client has initiated a connection")
+
+    // Handle incoming messages in a separate goroutine
+    go func() {
+        for {
+            _, msg, err := conn.ReadMessage()
+            if err != nil {
+                log.Error().Err(err).Msg("Error reading message")
+                break
+            }
+            
+            log.Debug().Str("message", string(msg)).Msg("Message received")
+
+            // Echo message back to client
+            err = conn.WriteMessage(websocket.TextMessage, msg)
+            if err != nil {
+                log.Error().Err(err).Msg("Error writing message")
+                break
+            }
+        }
+    }()
+
+    select {}
+}
