31d80da267cbf8eb725333a12f6c45d358548093
NotAditya69 <90441096+NotAditya69@users.noreply.github.com>
2024-05-30 18:04:07 +0530

    feat(latest): updates

--- cmd/main.go
+++ cmd/main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-    http.HandleFunc("/v1/ws", socket.HandleWebSocket)
+    http.HandleFunc("/ws/v1", socket.HandleWebSocket)
     log.Info().Msg("Server started on port 6001")
     http.ListenAndServe(":6001", nil)
 }
