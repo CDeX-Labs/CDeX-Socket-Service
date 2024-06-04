557e810cb7ac6e3b9ccbce416b0a616f0f6c45ab
NotAditya69 <90441096+NotAditya69@users.noreply.github.com>
2024-06-04 20:09:17 +0530

    fix(socket): improve logic and reasoning

--- pkg/socket/socket.go
+++ pkg/socket/socket.go
@@ -1,64 +1,45 @@
 package socket
 
 import (
-    "net/http"
-    "sync"
-    "github.com/gorilla/websocket"
-    "github.com/rs/zerolog/log"
+	"github.com/gorilla/websocket"
+	"github.com/rs/zerolog/log"
+	"net/http"
+	"sync"
 )
 
 var upgrader = websocket.Upgrader{
-    CheckOrigin: func(r *http.Request) bool {
-        return true
-    },
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
 }
 
 var clients = make(map[*websocket.Conn]bool)
 var clientsLock = sync.RWMutex{}
 
 func HandleWebSocket(w http.ResponseWriter, r *http.Request) {
-    conn, err := upgrader.Upgrade(w, r, nil)
-    if err != nil {
-        log.Error().Err(err).Msg("Error upgrading connection to websocket")
-        return
-    }
-    defer conn.Close()
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade failed: ", err)
+		return
+	}
+	defer conn.Close()
 
-    log.Debug().Msg("Client has initiated a connection")
-
-    clientsLock.Lock()
-    clients[conn] = true
-    clientsLock.Unlock()
-
-    defer func() {
-        clientsLock.Lock()
-        delete(clients, conn)
-        clientsLock.Unlock()
-    }()
-
-    go func() {
-        for {
-            _, msg, err := conn.ReadMessage()
-            if err != nil {
-                log.Error().Err(err).Msg("Error reading message")
-                break
-            }
-
-            log.Debug().Str("message", string(msg)).Msg("Message received")
-
-            clientsLock.RLock()
-            for client := range clients {
-                if client != conn {
-                    err = client.WriteMessage(websocket.TextMessage, msg)
-                    if err != nil {
-                        log.Error().Err(err).Msg("Error writing message")
-                        break
-                    }
+    // Echo incoming messages to all clients
+    for {
+        messageType, p, err := conn.ReadMessage()
+        if err != nil {
+            log.Debug().Msg("Error reading message")
+            return
+        }
+        clientsLock.RLock()
+        for client := range clients {
+            if client != conn {
+                if err := client.WriteMessage(messageType, p); err != nil {
+                    log.Debug().Msg("Error writing message")
+                    return
                 }
             }
-            clientsLock.RUnlock()
         }
-    }()
-
-    select {}
+        clientsLock.RUnlock()
+    }
 }
